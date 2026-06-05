cae555bafb570c487b627596d8d4834ed0aa14eb
fskelly <fletcher_kelly@outlook.com>
2026-06-05 11:00:27 +0100

    Update site URL in astro.config.mjs and add netlify.toml for deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
+	site: "https://me.fskelly.com/",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
